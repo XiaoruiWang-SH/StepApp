e72e6e777d5ddef2c3919114113a764441d8184e
DNsrc <83851675+teinei698@users.noreply.github.com>
2024-12-03 11:19:12 +0100

    add implementation layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,6 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
-    implementation(libs.constraintlayout)
     implementation(libs.lifecycle.livedata.ktx)
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.navigation.fragment)
